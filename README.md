01ca7603f6c4176b83ee4252e21a8b6d4c980988
Tyson Andre <tysonandre775@hotmail.com>
2018-05-13 18:11:49 -0700

    WIP (Broken) Start working on changing this to install/run phan instead of php-language-server.
    Rename everything to avoid conflicting with
    https://github.com/emacs-lsp/lsp-php
    
    TODO: Take code and the main configuration options
    from https://github.com/tysonandre/vscode-php-phan
    and rewrite those in emacs lisp.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# lsp-php
+# lsp-phan (wip)
 
-PHP support for [lsp-mode](https://github.com/emacs-lsp/lsp-mode) using [felixfbecker/php-language-server](https://github.com/tszg/lsp-php#installation).
+# THIS IS NOT WORKING YET
+
+PHP (Phan) support for [lsp-mode](https://github.com/emacs-lsp/lsp-mode) using [felixfbecker/php-language-server](https://github.com/TysonAndre/lsp-phan#installation).
+
+This is forked from [emacs-lsp/lsp-php](https://github.com/emacs-lsp/lsp-php). (The way to determine the root Phan namespace is different)
 
 :warning: **This package is not currently available in MELPA. Please refer to the documentation in [tszg/lsp-php](https://github.com/tszg/lsp-php) for configuring the MELPA-installed `php-lsp` until this issue is resolved.** :warning:
 
@@ -9,9 +13,10 @@ PHP support for [lsp-mode](https://github.com/emacs-lsp/lsp-mode) using [felixfb
 ### Emacs
 
 Clone this repository, [emacs-lsp/lsp-mode](https://github.com/emacs-lsp/lsp-mode), and [emacs-lsp/lsp-ui](https://github.com/emacs-lsp/lsp-ui) to suitable paths. Then add the following settings to your Emacs configuration.
+(TODO: If you also wish to install `lsp-php` for other features such as autocomplete, are multiple language servers for the same language supported?)
 
 ```emacs-lisp
-(add-to-list 'load-path "<path to lsp-php>")
+(add-to-list 'load-path "<path to lsp-phan>")
 (add-to-list 'load-path "<path to lsp-mode>")
 (add-to-list 'load-path "<path to lsp-ui>")
 
@@ -20,32 +25,15 @@ Clone this repository, [emacs-lsp/lsp-mode](https://github.com/emacs-lsp/lsp-mod
 (require 'lsp-ui)
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
 
-(require 'lsp-php)
+(require 'lsp-phan)
 (add-hook 'php-mode-hook #'lsp-mode)
 ```
 
-### php-language-server
+### phan installation
 
-You will need to install [felixfbecker/php-language-server](https://github.com/felixfbecker/php-language-server). Package defaults assume it is either installed in `~/.emacs.d/php-language-server/vendor/felixfbecker/language-server` or installed globally via Composer.
+You will need to install [phan/phan](https://github.com/phan/phan). Package defaults assume it is either installed in `~/.emacs.d/php-language-server/vendor/phan/phan` or installed globally via Composer.
 
-#### Local installation
-
-Create a directory for php-language-server in `.emacs.d/php-language-server`. Create a `composer.json` file in it, with the following contents:
-
-```json
-{
-    "minimum-stability": "dev",
-    "prefer-stable": true
-}
-
-```
-
-Then, in the directory, run the following commands:
-
-```shell
-composer require felixfbecker/language-server
-composer run-script --working-dir=vendor/felixfbecker/language-server parse-stubs
-```
+You will also need to set up the project with a `.phan/config.php` if you have not done so already.
 
 #### Global installation
 
@@ -68,16 +56,10 @@ composer global run-script --working-dir=vendor/felixfbecker/language-server par
 
 ## Workspace root detection
 
-By default, `lsp-php` determines the workspace root by the following detectors (in order.)
+By default, `lsp-php` determines the workspace root by the following detector (in order.)
 
-- `lsp-php-root-composer-json`: Find a directory that contains a `composer.json`, but is not two levels under `vendor`. I.e. for the path `/your/project/vendor/felixfbecker/language-server/src/` this would return `/your/project`, assuming it contains a `composer.json`.
-- `lsp-php-root-projectile`: Use the current Projectile project as the root directory.
-- `lsp-php-root-vcs`: Use the current version control system root as the project.
-- `".dir-locals.el"`: Use the nearest parent directory (or self) containing a `.dir-locals.el` file.
-- `".project"`: Use the nearest parent directory (or self) containing a `.project` file.
-- `"index.php"`: Use the nearest parent directory (or self) containing an `index.php` file.
-- `"robots.txt"`: Use the nearest parent directory (or self) containing a `robots.txt` file.
 - Default to `default-directory`.
+- `.phan/config.php`: Find a directory that contains a `.phan/config.php`
 
 The order of the detectors is configurable. You can also specify additional files to look for in project directories.
 
@@ -85,21 +67,18 @@ The order of the detectors is configurable. You can also specify additional file
 
 You can configure `lsp-php` with the following customization options:
 
+TODO: Base it on the VS code configuration options.
+
 | Option | Description |
 | ------ | ----------- |
-| `lsp-php-server-install-dir` | Directory of a Composer project requiring `felixfbecker/language-server`. Defaults to `~/.emacs.d/php-language-server`. If it is not accessible, defaults to `~/.config/composer` (for globally required php-language-server). |
-| `lsp-php-language-server-command` | Command to run php-language-server-with. Separate arguments Should be separate entries in the list. Defaults to `(list "php" (expand-file-name "vendor/bin/php-language-server.php" lsp-php-server-install-dir))` |
-| `lsp-php-show-file-parse-notifications` | If `nil`, hide the `Parsing file:///var/www/index.php` and `Restored monolog/monolog:1.23.0 from cache` messages. Defaults to `t`. |
-| `lsp-php-workspace-root-detectors` | List of strings naming dominating, or the special symbols (not strings) `lsp-php-root-vcs`, `lsp-php-root-projectile`, and `lsp-php-root-composer-json`. The detectors are evaluated in order, and the first one with a match will determine the workspace root. Defaults to `(quote (lsp-php-root-composer-json lsp-php-root-projectile lsp-php-root-vcs ".dir-locals.el" ".project" "index.php" "robots.txt"))` |
+| `lsp-phan-server-install-dir` | Directory of a Composer project requiring `felixfbecker/language-server`. Defaults to `~/.emacs.d/php-language-server`. If it is not accessible, defaults to `~/.config/composer` (for globally required php-language-server). |
+| `lsp-phan-language-server-command` | Command to run php-language-server-with. Separate arguments Should be separate entries in the list. Defaults to `(list "php" (expand-file-name "vendor/bin/php-language-server.php" lsp-php-server-install-dir))` |
 
 ```emacs
 (custom-set-variables
   ;; Run php-language server with a different php interpreter and from a different location.
-  '(lsp-php-language-server-command (quote ("/usr/local/bin/php7" "/opt/php-language-server/bin/php-language-server.php")))
-  ;; Hide noisy messages when opening a large project
-  '(lsp-php-show-file-parse-notifications nil)
-  ;; Composer.json detection after Projectile.
-  '(lsp-php-workspace-root-detectors (quote (lsp-php-root-projectile lsp-php-root-composer-json "index.php" "robots.txt")))
+  ;; TODO: Figure out what a valid command is
+  '(lsp-php-language-server-command (quote ("/usr/local/bin/php7" "/opt/phan/phan" "--language-server-on-stdin")))
 )
 
 ```
--- lsp-php.el
+++ lsp-php.el
@@ -1,13 +1,16 @@
-;;; lsp-php.el --- PHP support for lsp-mode -*- lexical-binding: t -*-
+;;; lsp-phan.el --- PHP (Phan) support for lsp-mode -*- lexical-binding: t -*-
+;;; TODO: Finish implementing this
 
-;; Copyright (C) 2017-2018 zg, Declspeck
+;; Copyright (C) 2018 Tyson Andre, (C) 2017-2018 zg, Declspeck
 
-;; Author: Declspeck <declspeck@declblog.com>
-;;         zg <13853850881@163.com>
+;; Author: Tyson Andre
 ;; Maintainer: Declspeck <declspeck@declblog.com>
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "25.1") (lsp-mode "3.4"))
-;; URL: https://github.com/emacs-lsp/lsp-php
+;; URL: https://github.com/emacs-lsp/lsp-phan
+
+;; Based on lsp-php by Declspeck <declspeck@declblog.com> and zg <13853850881@163.com>
+;;  (https://github.com/emacs-lsp/lsp-php)
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,89 +27,80 @@
 
 ;;; Commentary:
 
-;; Adds PHP support to lsp-mode with php-language-server (felixfbecker/language-server )
+;; Adds PHP support to lsp-mode with phan (phan/phan)
 
 ;;; Code:
 
 (require 'lsp-mode)
 
-(defgroup lsp-php nil
-  "´lsp-php´ integrates php-language-server with ´lsp-mode´."
+(defgroup lsp-phan nil
+  "´lsp-phan´ integrates Phan with ´lsp-mode´."
   :group 'tools
   :group 'convenience)
 
-(defun lsp-php-find-php-language-server-install-dir ()
-    "Return the default installation dir of php-language-server."
-    (let ((default-dir (locate-user-emacs-file "php-language-server/")))
+;(defun lsp-phan-find-php-language-server-install-dir ()
+;    "Return the default installation dir of php-language-server."
+;    (let ((default-dir (locate-user-emacs-file "php-language-server/")))
+;      (seq-find 'file-accessible-directory-p (list default-dir "~/.config/composer") default-dir)))
+(defun lsp-phan-find-phan-install-dir ()
+    "Return the default installation dir of phan."
+    (let ((default-dir (locate-user-emacs-file "phan/")))
       (seq-find 'file-accessible-directory-p (list default-dir "~/.config/composer") default-dir)))
 
-(defcustom lsp-php-server-install-dir (lsp-php-find-php-language-server-install-dir)
+(defcustom lsp-phan-server-install-dir (lsp-phan-find-php-language-server-install-dir)
            "Install directory for php-language-server.
 This should point to the root of a Composer project requiring
-felixfbecker/language-server. If lsp-php-language-server-command is overridden,
+felixfbecker/language-server. If lsp-phan-language-server-command is overridden,
  this is setting has no effect."
-           :group 'lsp-php
+           :group 'lsp-phan
            :risky t
            :type 'directory)
 
-(defcustom lsp-php-language-server-command nil
+(defcustom lsp-phan-language-server-command nil
            "Command to run php-language-server with.
-If nil, use lsp-php-server-install-dir and the php in path."
+If nil, use lsp-phan-server-install-dir and the php in path."
            :type '(repeat (string))
-           :group 'lsp-php)
+           :group 'lsp-phan)
 
-(defcustom lsp-php-show-file-parse-notifications t
-           "Show the \"Parsing file\" and \"Restored from cache\" messages."
-           :type 'boolean
-           :group 'lsp-php)
-
-(defcustom lsp-php-workspace-root-detectors
-           '(lsp-php-root-composer-json
-             lsp-php-root-projectile
-             lsp-php-root-vcs
+(defcustom lsp-phan-workspace-root-detectors
+           '(lsp-phan-root-composer-json
+             lsp-phan-root-projectile
+             lsp-phan-root-vcs
              ".dir-locals.el"
              ".project"
              "index.php"
              "robots.txt")
            "How to detect the project root. Selected methods are tried in the order they are specified."
            :type '(repeat (choice
-                            (const  :tag "Contains composer.json"      lsp-php-root-composer-json)
-                            (const  :tag "Projectile root"             lsp-php-root-projectile)
-                            (const  :tag "Version control system root" lsp-php-root-vcs)
+                            (const  :tag "Contains composer.json"      lsp-phan-root-composer-json)
+                            (const  :tag "Projectile root"             lsp-phan-root-projectile)
+                            (const  :tag "Version control system root" lsp-phan-root-vcs)
                             (string :tag "Contains a named file")))
-           :group 'lsp-php)
+           :group 'lsp-phan)
 
-(defun lsp-php-parent (path)
+(defun lsp-phan-parent (path)
   "For PATH a/b/ or a/b return a\/. 'nil is passed through."
   (when path
     (file-name-directory
       (directory-file-name
         (expand-file-name path)))))
 
-(defun lsp-php-basename (path)
+(defun lsp-phan-basename (path)
   "For PATH a/b/ or a/b return b. 'nil is passed through."
   (when path
     (file-name-nondirectory
       (directory-file-name
         (expand-file-name path)))))
 
-(defun lsp-php-root-vcs ()
-  "Return the project directory, as determined by VCS, if any."
-  (when (vc-backend default-directory) (vc-root-dir)))
-
-(defun lsp-php-is-composer-json-root (dir)
-  "Check if DIR contains composer.json and is not a vendor package."
+(defun lsp-phan-is-phan-root (dir)
+  "Check if DIR contains .phan and is not a vendor package."
   (let ((expanded-dir (expand-file-name dir)))
-    (and (file-exists-p (expand-file-name "composer.json" expanded-dir))
-         (let* ((grandparent (lsp-php-parent (lsp-php-parent expanded-dir)))
-                (basename-of-grandparent (lsp-php-basename grandparent)))
+    (and (file-exists-p (expand-file-name ".phan" expanded-dir))
+         (let* ((grandparent (lsp-phan-parent (lsp-phan-parent expanded-dir)))
+                (basename-of-grandparent (lsp-phan-basename grandparent)))
                 (not (equal "vendor" basename-of-grandparent))))))
 
-(defun lsp-php-root-composer-json ()
-  "Return the parent directory containing composer.json, but which is not a vendor package."
-  (locate-dominating-file default-directory 'lsp-php-is-composer-json-root))
-
-(defun lsp-php-root-projectile ()
+(defun lsp-phan-root-projectile ()
   "Return the projectile root, if any."
   (and
    (fboundp 'projectile-project-p)
@@ -114,40 +108,43 @@ If nil, use lsp-php-server-install-dir and the php in path."
    (projectile-project-p)
    (projectile-project-root)))
 
-(defun lsp-php-get-root ()
-  "Find workspace root as specified by ´lsp-php-workspace-root-detectors´. Defaults to ´default-directory´."
+(defun lsp-phan-get-root ()
+  "Find workspace root as specified by ´lsp-phan-workspace-root-detectors´. Defaults to ´default-directory´."
   (expand-file-name
     (or (seq-some (lambda (filename-or-function)
                     (if (stringp filename-or-function)
                         (locate-dominating-file default-directory filename-or-function)
                         (funcall filename-or-function)))
-                  lsp-php-workspace-root-detectors)
+                  lsp-phan-workspace-root-detectors)
         (progn (message "Couldn't find project root, using the current directory as the root.")
                default-directory))))
 
-(defun lsp-php-get-ignore-regexps ()
+; TODO: Not applicable to phan, remove
+(defun lsp-phan-get-ignore-regexps ()
   "Return the list of regexps to filter php-language-server output with."
-  (unless lsp-php-show-file-parse-notifications
+  (unless lsp-phan-show-file-parse-notifications
     '("\"message\":\"Parsing file:"
       "\"message\":\"Restored .*from cache")))
 
-; This applies default to lsp-php-language-server-command.
+; This applies default to lsp-phan-language-server-command.
 ; The default cannot be applied in defcustom, since it would depend on the value
-; of another defcustom, lsp-php-server-install-dir.
-(defun lsp-php-get-language-server-command ()
+; of another defcustom, lsp-phan-server-install-dir.
+
+; FIXME: finish adding arguments to this command
+(defun lsp-phan-get-language-server-command ()
   "Return the command to run php-language-server with."
-  (or lsp-php-language-server-command
+  (or lsp-phan-language-server-command
       (list "php" (expand-file-name
-                   "vendor/felixfbecker/language-server/bin/php-language-server.php"
-                   lsp-php-server-install-dir))))
+                   "vendor/bin/phan"
+                   lsp-phan-server-install-dir))))
 
-(lsp-define-stdio-client lsp-php "php"
-                         'lsp-php-get-root
-                         (lsp-php-get-language-server-command)
-                         :ignore-regexps (lsp-php-get-ignore-regexps))
+(lsp-define-stdio-client lsp-phan "php"
+                         'lsp-phan-get-root
+                         (lsp-phan-get-language-server-command)
+                         :ignore-regexps (lsp-phan-get-ignore-regexps))
 
-(provide 'lsp-php)
+(provide 'lsp-phan)
 
 ; vim: ff=unix:sw=2:ts=2:tw=0
 
-;;; lsp-php.el ends here
+;;; lsp-phan.el ends here
